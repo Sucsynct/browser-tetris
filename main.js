6f29d2883e739c0566f7c2eb965d4e543de22660
Sucsynct <sucsynct@gmail.com>
2017-08-25 17:16:29 +0200

    Enumerate blocks

--- main.js
+++ main.js
@@ -4,15 +4,49 @@ var width = canvas.width = 240;
 var height = canvas.height = 400;
 var offset = {};
 
-const testMatrix = [
-  [0, 0, 0],
-  [1, 1, 1],
-  [0, 1, 0]
-];
+const blocks = {
+  I_block: [
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0]
+  ],
+  J_block: [
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 0]
+  ],
+  L_block: [
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 1]
+  ],
+  O_block: [
+    [0, 0, 0],
+    [1, 1, 0],
+    [1, 1, 0]
+  ],
+  Z_block: [
+    [0, 0, 0],
+    [1, 1, 0],
+    [0, 1, 1]
+  ],
+  T_block: [
+    [0, 0, 0],
+    [0, 1, 0],
+    [1, 1, 1]
+  ],
+  S_block: [
+    [0, 0, 0],
+    [0, 1, 1],
+    [1, 1, 0]
+  ]
+};
 
-function Player(x, y, matrix, offset) {
+function Player(matrix, offset) {
+  let x = canvas.width/2;
   this.x = x + offset.x
-  this.y = y + offset.y
+  this.y = 0 + offset.y
   this.matrix = matrix;
 }
 
@@ -31,7 +65,7 @@ function draw() {
   requestAnimationFrame(draw);
   ctx.fillStyle = '#000000';
   ctx.fillRect(0, 0, width, height);
-  drawMatrix(testMatrix);
+  drawMatrix(blocks.J_block);
 }
 
 function playerDrop() {
